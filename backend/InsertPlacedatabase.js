f9bfe684676033c5333f5b07cf20b17eb9a00963
Oseias Beu <oseiasbeu@outlook.com>
2019-09-17 11:59:25 -0300

    UPDATE DATABASE COLLECTION

--- backend/InsertPlacedatabase.js
+++ backend/InsertPlacedatabase.js
@@ -1,10 +1,18 @@
 let MongoClient = require('mongodb').MongoClient;
-
-
 let url = 'mongodb+srv://admin:26031998boxe@@cluster0-b3pli.mongodb.net/IMTS'
 console.log(`=================================================`)
 console.log(`INSERINDO CADEIRAS!!!`)
 console.log(`=================================================`)
+
+
+// apoio[{
+//     _id:  Xeeee!,
+//     cordinates: [x,y],
+//     occupation: Boolean
+// }]
+
+
+
 
 MongoClient.connect(url, {
     useNewUrlParser: true,
@@ -16,10 +24,10 @@ MongoClient.connect(url, {
     //return
     for (let cadeira = 1; cadeira <= 100; cadeira++) {
         // dictionaryobj.Add(key, item)
-        db.collection('apoio').insertOne({ H: cadeira, occupation: false },
+        db.collection('apoio').insertOne({ cordinates:["B",cadeira], occupation: false },
             function (err, res) {
                 // if (err) throw err;
-                console.log(`Cadeira:${cadeira} do Bloco:H inserida com sucesso na base!`);
+                console.log(`Cadeira:${cadeira} do Bloco:B inserida com sucesso na base!`);
             });
         
     }       
--- frontend/index.js
+++ frontend/index.js
@@ -1,32 +1,26 @@
 let blocos = document.querySelector('#BLOCOS');
+let img = new Image();
 let cadeiras = 'http://localhost:3000/ChoicePlace'
-
 let caminhoCadeiraImg = '/imgs/poltrona.svg'
+
 
 axios.get(cadeiras).then(function (response) {
     console.log(Object.entries(response.data[1])[2])
     for (let cadeira = 1; cadeira < response.data.length; cadeira ++) {
         console.log(response.data[cadeira])
-        blocos.innerHTML = +`<article>${Object.entries(response.data[cadeira])[1]}</article>`;
+        // blocos.innerHTML = +`<article>${Object.entries(response.data[cadeira])[1]}</article>`;
     }
     // console.log(listarTodasAsPropriedades(response.data))
 });
 
-blocos.innerHTML =+ `<img src=${caminhoCadeiraImg} alt="cadeira imagem"/>`
 
+// INSERT IMAGENS IN DISPLAY
+// img.onload = function() {
+//   blocos.appendChild(img);
+// };
 
-// for(row in response.data) {
-//     console.log('array["' + row + '"] = ' + response.data[row]);    
-// }
-
-// function listarTodasAsPropriedades(o){     
-// 	let objectoASerInspecionado;     
-// 	let resultado = [];
-	
-// 	for(objectoASerInspecionado = o; objectoASerInspecionado !== null; objectoASerInspecionado = Object.getPrototypeOf(objectoASerInspecionado)){  
-// 		resultado = resultado.concat(Object.getOwnPropertyNames(objectoASerInspecionado));  
-// 	}
-	
-// 	return resultado; 
-// }
-
+img.onload = function() {
+    blocos.innerHTML =+ `<img src=${img.src}    />`
+  };
+  
+img.src = caminhoCadeiraImg;

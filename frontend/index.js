3c952217b7ed0f04347740ff1909a0162a39b8c5
Oseias Beu <oseiasbeu@outlook.com>
2019-09-17 15:13:49 -0300

    ORGANIZED REQUEST FRON SIDE

--- frontend/index.js
+++ frontend/index.js
@@ -1,27 +1,44 @@
-let blocos = document.querySelector('#BLOCOS');
-let img = new Image();
 let cadeiras = 'http://localhost:3000/ChoicePlace'
+let blocos = document.querySelector('#templo');
+let BA = document.querySelector('#blocoA')
+let img = new Image();
+
 let caminhoCadeiraImg = '/imgs/poltrona.svg'
 
+// axios({
+//         method: 'get',
+//         url: 'http://localhost:3000/ChoicePlace',
+//         // responseType: 'stream'
+//     })
+//     .then(function(response) {
+//         console.log(response.data[0].cordinates)
+//     });
 
-axios.get(cadeiras).then(function (response) {
-    // console.log(Object.entries(response.data)
-    // console.log(response.data)   
-    for (let cadeira = 1; cadeira < response.data.length; cadeira ++) {
-        console.log(response.data[cadeira].cordinates +  response.data[cadeira].occupation)
-        // blocos.innerHTML = +`<article>${Object.entries(response.data[cadeira])[1]}</article>`;
-    }  
-    // console.log(listarTodasAsPropriedades(response.data))
-});
+async function getPlaces() {
+    try {
+        const response = await axios.get('http://localhost:3000/ChoicePlace');
+        let lugares = response.data;
+        for (let NC = 1; NC < lugares.length; NC++) {
+            console.log(`Bloco: ${lugares[NC].cordinates[0]} Cadeira Nº: ${lugares[NC].cordinates[1]}`);
+        }
+        console.log(`Lugares carregados com sucesso!`)
+        return lugares;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 
-// INSERT IMAGENS IN DISPLAY
-// img.onload = function() {
-//   blocos.appendChild(img);
-// };
+// function tagPlace() {
+//     let pl = getPlaces();
+//     console.log(pl);
+//     for (let NC = 1; NC < pl.length; NC++) {
+//         console.log(`Bloco: ${pl.cordinates[0]}`);
+//     }
+// }
 
-// img.onload = function() {
-//     blocos.innerHTML =+ `<img src=${img.src}    />`
-//   };
-  
-// img.src = caminhoCadeiraImg;
+
+getPlaces();
+
+// tagPlace();
---
+++ frontend/teste.js
@@ -0,0 +1,7 @@
+let cadeira = {
+    id: 123142,
+    cordenadas: ["A", 1],
+    ocupacao: false
+}
+
+console.log(`Bloco: ${cadeira.cordenadas[0]} Cadeira Nº: ${cadeira.cordenadas[1]}`);

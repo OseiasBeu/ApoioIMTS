83f29a928394414e5b2c8c13e7ef8d822f64b2f8
Oseias Beu <oseiasbeu@outlook.com>
2019-09-18 14:44:08 -0300

    INCLUDE GRID RESPONSIVE

---
+++ frontend/responsive.js
@@ -0,0 +1,43 @@
+let BlocoA = document.querySelector('#blocoA');
+let BlocoB = document.querySelector('#blocoB');
+let BlocoC = document.querySelector('#blocoC');
+let BlocoD = document.querySelector('#blocoD');
+
+function ativaBlocoA() {
+    BlocoA.style.display = 'flex';
+    BlocoB.style.display = 'none';
+    BlocoC.style.display = 'none';
+    BlocoD.style.display = 'none';
+}
+
+
+function ativaBlocoA() {
+    BlocoA.style.display = 'flex';
+    BlocoB.style.display = 'none';
+    BlocoC.style.display = 'none';
+    BlocoD.style.display = 'none';
+}
+
+
+function ativaBlocoB() {
+    BlocoB.style.display = 'flex';
+    BlocoA.style.display = 'none';
+    BlocoC.style.display = 'none';
+    BlocoD.style.display = 'none';
+}
+
+
+function ativaBlocoC() {
+    BlocoC.style.display = 'flex';
+    BlocoB.style.display = 'none';
+    BlocoA.style.display = 'none';
+    BlocoD.style.display = 'none';
+}
+
+
+function ativaBlocoD() {
+    BlocoD.style.display = 'flex';
+    BlocoB.style.display = 'none';
+    BlocoC.style.display = 'none';
+    BlocoA.style.display = 'none';
+}
